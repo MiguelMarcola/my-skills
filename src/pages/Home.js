84ee826b9718e5c33a29fb1b823b2819eefd9567
MiguelMarcola <miguelmarcola@gmail.com>
2021-12-31 13:32:26 -0300

    add components and making layout

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,73 @@
-import React from "react";
-import { View, Text } from 'react-native'
+import React, { useState } from "react";
+import { View, 
+        Text, 
+        StyleSheet, 
+        SafeAreaView, 
+        TextInput, 
+        Platform
+} from 'react-native'
+
+import { Button } from "./components/Button";
+import { SkillCard } from "./components/SkillCard";
 
 export function Home(){
+  const [newSkill, setNewSkill] = useState('');
+  const [mySkills, setMySkills] = useState([]);
+
+  function handleAddNewSkill(){
+    setMySkills(oldState => [...oldState, newSkill]);
+  }
+
   return(
-    <View style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        }}>
-        <Text>React Native</Text>
-        <Text>Miguel Marçola</Text>
+    <View style={styles.container}>
+        <Text style={styles.title}>
+          Welcome, Miguel
+        </Text>
+
+        <TextInput 
+          style={styles.input}
+          placeholder="New skill"
+          placeholderTextColor='#555'
+          onChangeText={setNewSkill}
+        />
+
+       <Button onPress={handleAddNewSkill}/>
+
+
+        <Text style={[styles.title, {marginVertical: 30,}]}>
+          My Skills
+        </Text>
+
+        {
+          mySkills.map(skill => (
+            <SkillCard skill={skill}/>
+          ))
+        }
+
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    backgroundColor: '#121015',
+    paddingHorizontal: 30,
+    paddingVertical: 30,
+  },
+
+  title:{
+    color: '#fff',
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+
+  input:{
+    backgroundColor: '#1f1e25',
+    color: '#fff',
+    fontSize: 18,
+    padding: Platform.OS === 'ios' ? 15 : 10,
+    marginTop: 30,
+    borderRadius: 7,
+  },
+})
---
+++ src/pages/components/SkillCard.js
@@ -0,0 +1,37 @@
+import React from "react";
+import {
+    TouchableOpacity, 
+    Text,
+    StyleSheet
+} from 'react-native';
+
+export function SkillCard({skill}){
+    return(
+        <TouchableOpacity 
+            key={skill}
+            style={styles.buttonSkill}
+            activeOpacity={.4}
+        >
+            <Text style={styles.textSkill}>
+            {skill}
+              </Text>
+        </TouchableOpacity>
+    )
+}
+
+const styles = StyleSheet.create({
+    buttonSkill:{
+      backgroundColor: '#242422',
+      padding: 10,
+      borderRadius: 40,
+      alignItems: 'center',
+      marginBottom: 15,
+    },
+  
+    textSkill:{
+      color: '#fff',
+      fontSize: 20,
+      fontWeight: 'bold',
+      paddingVertical: 5,
+    }
+  })
